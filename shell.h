2250483ceb10771a1a829f748c569d16e807892c
Geff115 <gabrielnoah129@gmail.com>
2023-12-13 23:59:53 +0100

    Added the header file

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+#define ARG_MAX 256
+#define delimeter ":"
+
+#endif
